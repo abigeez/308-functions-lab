16ac3306196530035b7d65f9d3d610039b3eeb5c
Abi Garner <abigail.gee@hotmail.com>
2024-04-20 23:59:52 -0400

    second question still working

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-//part 1 thinking functionally
+//part 1: thinking functionally///
 
-let arrnum =[5,10,15,20]
+///////take an array of numbers and return the sum //////////////
+
+
+let arrnum =[5,10,15,20];
 function addArr(arrnum) {
     let sum = 0;
     for(let i=0; i<arrnum.length;i++){
@@ -8,6 +11,29 @@ function addArr(arrnum) {
     }
     return sum;
 }
+const addedUp= addArr(arrnum);
+console.log(addedUp)
 
-console.log(addArr(arrnum))
-//////
+
+///// take an array of numbers and return the average//////
+
+let meNum = [30,40,50,60,70];
+indNum= meNum.length;
+// let indNum = meNum.length;
+// // let avgOf= 
+
+function meanArr(meNum,indNum) {
+    const meanArr = (meNum/indNum)
+    let sum1= 0;
+for (let j=0;j<meNum.length;j++){
+    sum1 =+ meNum[j]
+   
+    
+}
+return (sum1/indNum)
+}
+
+
+
+console.log(meanArr(meNum,indNum))
+
